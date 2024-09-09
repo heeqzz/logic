2b6b5fb2f4e89d2afdf10c96c475fe11229214d8
Vladislav <geras15112005@yandex.ru>
2024-09-09 21:13:52 +0300

    add 2

---
+++ лаб 1/2/2.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	srand(time(NULL));
+	int w, v[10];
+	for (int i = 0; i < 10; i++) {
+		w = rand() % 100;
+		v[i] = w;
+		printf("%d ", v[i]);
+	}
+	getchar();
+}
+

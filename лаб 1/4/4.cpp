b6cd202295b4e077aa4b1184da3c31928f750932
Vladislav <geras15112005@yandex.ru>
2024-09-09 21:46:34 +0300

    add 4

---
+++ лаб 1/4/4.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	srand(time(NULL));
+	int w, v[5][5], n = 5, sum[5]{};
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			w = rand() % 100;
+			v[i][j] = w;
+			printf("%d     ", v[i][j]);
+		}
+		printf("\n");
+	}
+	for (int j = 0; j < n; j++) {
+		for (int i = 0; i < 5; i++) {
+			sum[j] += v[i][j];
+
+		}
+	}
+	
+	for (int j = 0; j < n; j++) {
+		printf("%d    ", sum[j]);
+	}
+	
+	getchar();
+}

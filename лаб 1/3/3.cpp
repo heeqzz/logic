459c23e9dc8cc6b9d4ed77e842fc41df29f00e65
Vladislav <geras15112005@yandex.ru>
2024-09-09 21:48:42 +0300

    add 3

---
+++ лаб 1/3/3.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	srand(time(NULL));
+	int w, *v, n;
+	printf("enter n\n");
+	scanf_s("%d", &n);
+	getchar();
+	v=(int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++) {
+		w = rand() % 100;
+		v[i] = w;
+		printf("%d ", v[i]);
+	}
+	free(v);
+	getchar();
+}

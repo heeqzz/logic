7cf98f32527cef66294ffe922b8e100304169c0c
Vladislav <geras15112005@yandex.ru>
2024-10-03 20:57:09 +0300

    zadanie 3

---
+++ лаб 3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,91 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE  
+#define _CRT_NONSTDC_NO_DEPRECATE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+struct node
+{
+	char inf[256];  // полезная информация
+	struct node* next; // ссылка на следующий элемент 
+};
+struct node* head = NULL, * last = NULL, * f = NULL; // указатели на первый и последний элементы списка
+char specchar;
+int g = 0;
+int spstore();
+void review(void), del(char* name);
+char find_el[256];
+struct node* find(char* name); // функция нахождения элемента
+struct node* get_struct(void); // функция создания элемента
+struct node* get_struct(void)
+{
+	struct node* p = NULL;
+	char s[256];
+	int num = 0;
+	if ((p = (node*)malloc(sizeof(struct node))) == NULL)  // выделяем память под новый элемент списка
+	{
+		printf("Ошибка при распределении памяти\n");
+		exit(1);
+	}
+	printf("Введите название объекта: \n");   // вводим данные
+	scanf("%s", s);
+	if (*s == 0)
+	{
+		printf("Запись не была произведена\n");
+		return NULL;
+	}
+	strcpy(p->inf, s);
+	p->next = NULL;
+	return p;		// возвращаем указатель на созданный элемент
+}
+/* Последовательное добавление в список элемента (в конец)*/
+int spstore()
+{
+	struct node* p = NULL;
+	p = get_struct();
+	if (head == NULL && p != NULL)	// если списка нет, то устанавливаем голову списка
+	{
+		head = p;
+		last = p;
+	}
+	else if (head != NULL && p != NULL) // список уже есть, то вставляем в конец
+	{
+		p->next = head;//постоянно меняется голова(LIFO)
+		head = p;
+	}
+	printf("Продолжаем ввод? 1/0 \n");
+	scanf_s(" %c", &specchar);
+	getchar();
+	if (specchar == '1') {
+		g = 1;
+	}
+	else {
+		g = 0;
+	}
+	return (g);
+}
+/* Просмотр содержимого списка. */
+void review(void)
+{
+	struct node* struc = head;
+	if (head == NULL)
+	{
+		printf("Список пуст\n");
+	}
+	while (struc)
+	{
+		printf("Имя - %s \n", struc->inf);
+		struc = struc->next;
+	}
+	return;
+}
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	spstore();
+	while (g == 1) {
+		g = spstore();
+	}
+	review();
+}
